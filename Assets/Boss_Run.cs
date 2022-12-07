619cddd682ed12971eb30534d7c11e6fc713fae2
harry-04 <72386526+harry-04@users.noreply.github.com>
2022-12-07 14:57:05 +0000

    Enemy AI

---
+++ Assets/Boss_Run.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Run : StateMachineBehaviour
+{
+    public float speed = 2.5f;
+
+    Transform player;
+    Rigidbody2D rb;
+    ThingEnemy thingEnemy;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = animator.GetComponent<Rigidbody2D>();
+        thingEnemy = animator.GetComponent<ThingEnemy>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        thingEnemy.LookAtPlayer();
+
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.fixedDeltaTime);
+        rb.MovePosition(newPos);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+}
--- Assets/Scripts/Enemies/ThingEnemy.cs
+++ Assets/Scripts/Enemies/ThingEnemy.cs
@@ -6,6 +6,28 @@ public class ThingEnemy : MonoBehaviour
 {
     public int health = 100;
 
+    public Transform player;
+    public bool isFlipped = false;
+
+    public void LookAtPlayer()
+    {
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
+
     public GameObject deathEffect;
 
     public void TakeDamage (int damage)
